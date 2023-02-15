05acd3a34ce550d6ae1093bf7863506d31bdab17
Vikash <vikashchaturvedi100@gmail.com>
2023-02-15 11:37:54 +0530

    this is my first flask programm

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask
-app=__name__
-@app.route('/')
-def hello():
-    return "HELLO MY NAME IS VIKASH"
+app=Flask(__name__)
+
+#route
+@app.route("/")
+def hello_world():
+    return "Hello my name is vikash and i am biggest fan of mia khalifa and rajshree verma"
+if __name__=="__main__":
+    app.run(host="0.0.0.0",port=5000)
